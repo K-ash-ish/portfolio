2316edb673a858e1068087d6ba79073da83c2866
K-ash-ish <kashishsondhiya@gmail.com>
2023-07-30 23:11:53 +0530

    removed footer for now

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
       <body className="h-screen">
         <Header />
         {children}
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   );
